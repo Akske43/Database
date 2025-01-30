12c79da15baad9823148928e331cface27986369
Ajay Sharma <159602947+Akske43@users.noreply.github.com>
2025-01-30 13:13:42 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,21 @@
+const men = [
+    {
+        title: "Hoodie",
+        des: "Trillion Half Zipper Solid Sweatshirt for Me",
+        rate: "Rate: Rs 1500",
+        url: "./images/mens images/hoodie (2).webp",
+        category: "men",
+        discount: "999 (30% off)"
+    },
+    {
+        title: "Shoes",
+        des: "Men's Energy Afterburn Disruptor for Multiple",
+        rate: "Rate: Rs 1500",
+        url: "./images/shoes images/men.jpg",
+        category: "shoes",
+        discount: "999 (30% off)"
+    }
+    // Add more products...
+];
+
+export default men;
